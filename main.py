5cda3b3e2b4932b60d6cff10bb073981216d2191
oliver2214 <dima.tovstokor@yandex.ru>
2024-04-07 15:44:49 +0300

    Добавить первые 2 ручки get, без функционала

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from typing import Union
+from typing import Annotated, Optional, Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
 
 app = FastAPI()
+app.title = "Сервис баннеров"
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/user_banner")
+def user_banner(tag_id: int,
+                feature_id: int,
+                use_last_revision: bool = False,
+                token: Annotated[str | None, Header()] = None):
+
+    return {
+        "tag_id": tag_id,
+        "feature_id": feature_id,
+        "use_last_revision": use_last_revision,
+        "token": token
+    }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/banner")
+def banner(token: Annotated[str | None, Header()] = None,
+           feature_id: int = None,
+           tag_id: int = None,
+           limit: int = None,
+           offset: int = None):
+
+    return {
+        "token": token,
+        "feature_id": feature_id,
+        "tag_id": tag_id,
+        "limit": limit,
+        "offset": offset
+    }
