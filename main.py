f3a4a0a0d625cd13f624a93dbbbb9b871fc85628
oliver2214 <dima.tovstokor@yandex.ru>
2024-04-07 16:24:48 +0300

    Добавить еще 3 эндпоинта без функционала, только валидация

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Optional, Union
+from typing import Annotated
 
 from fastapi import FastAPI, Header
 from pydantic import BaseModel
@@ -7,11 +7,22 @@ app = FastAPI()
 app.title = "Сервис баннеров"
 
 
+class SBanner(BaseModel):
+    tag_ids: list = [0]
+    feature_id: int = 0
+    content: dict = {
+        "title": "some_title",
+        "text": "some_text",
+        "url": "some_url"
+    }
+    is_active: bool = True
+
+
 @app.get("/user_banner")
 def user_banner(tag_id: int,
                 feature_id: int,
                 use_last_revision: bool = False,
-                token: Annotated[str | None, Header()] = None):
+                token: Annotated[str, Header()] = "user_token"):
 
     return {
         "tag_id": tag_id,
@@ -22,7 +33,7 @@ def user_banner(tag_id: int,
 
 
 @app.get("/banner")
-def banner(token: Annotated[str | None, Header()] = None,
+def banner(token: Annotated[str, Header()] = "admin_token",
            feature_id: int = None,
            tag_id: int = None,
            limit: int = None,
@@ -35,3 +46,22 @@ def banner(token: Annotated[str | None, Header()] = None,
         "limit": limit,
         "offset": offset
     }
+
+
+@app.post("/banner")
+def post_banner(token: Annotated[str, Header()] = "admin_token",
+                banner: SBanner = None):
+    return banner
+
+
+@app.patch("/banner/{id}")
+def patch_banner(id: int,
+                 token: Annotated[str, Header()] = "admin_token",
+                 banner: SBanner = None):
+    return banner
+
+
+@app.delete("/banner/{id}")
+def delete_banner(id: int,
+                  token: Annotated[str, Header()] = "admin_token"):
+    return id
